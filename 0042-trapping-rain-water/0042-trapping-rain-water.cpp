48eaf4e66e603b9cf1894d05a7d2cc1d3c223799
Nikhil Singh Tomar <nikhiltomar962006@gmail.com>
2026-08-13 09:42:58 +0530

    Time: 0 ms (100%), Space: 26.1 MB (56.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -11,18 +11,14 @@ public:
               prev[i] = max;
             if(max<height[i]) max = height[i];
         }
-        //Next greatest element array
-        int next[n];
-        next[n-1]= -1;
+        //Next greatest element array -> prev as next
+
+       prev[n-1]= -1;
         max = height[n-1];
         for(int i = n-2 ; i>=0 ; i--) {
-            next[i]= max;
-            if(max<height[i]) max = height[i];
+            if (max < prev[i]) prev[i]= max;
+            if(max<height[i])  max = height[i];
         } 
-        // minimum array -> prev as minimum
-        for(int i =0 ; i<n ; i++){
-              prev[i] = min(prev[i], next[i]);
-        }
 
         //calculating water 
         int water = 0;
